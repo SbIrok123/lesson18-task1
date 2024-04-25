0f9015c42fe91893c0c271ecb282b75def38d067
dekhtiarenkoOleh <sbirok1232@gmail.com>
2024-04-25 14:43:29 -0400

    create main

--- lesson18-task1/lesson18-task1.cpp
+++ lesson18-task1/lesson18-task1.cpp
@@ -2,8 +2,20 @@
 //
 
 #include <iostream>
-
+#include "computer.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+	int N = 5;
+	computer* arr = new computer[N]
+	{
+		{"HP",1.2,6,false,20'000},
+		{"Acer",3.2,8,true,15'000},
+		{"Asus",2.4,16,false,45'000},
+		{"Dell",2.2,8,false,17'000},
+		{"Apple",3.2,32,false,78'000}
+	};
+	for (int i = 0; i < N; i++)
+	{
+		arr[i].showinfo();
+	}
 }
